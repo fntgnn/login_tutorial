d9c10ccded52013becf1a8b793dfaf4b963e1e04
Giovanni <gio.fon@gmail.com>
2017-03-13 12:15:34 +0100

    first commit

---
+++ config/passport.js
@@ -0,0 +1,56 @@
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
+
+module.exports = function(passport){
+
+  passport.serializeUser(function(user, done){
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
+      done(err, user);
+    });
+  });
+
+  passport.use('local-signup', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+  function(req, email, password, done){
+    process.nextTick(function(){
+      User.findOne({ email }, function(err, user){
+        if(err) { return done(err); }
+        if(user) { return done(null, false, req.flash('signupMessage','Email already in use')); }
+        else{
+          var newUser = new User();
+          newUser.email = email;
+          newUser.password = newUser.generateHash(password);
+
+          newUser.save(function(err){
+            if(err) { throw err; }
+            return done(null, newUser);
+          });
+        }
+      });
+    });
+  }));
+
+  passport.use('local-login', new LocalStrategy({
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true
+    },
+    function(req, email, password, done){
+      User.findOne({ email }, function(err, user){
+        if(err) { return done(err); }
+        if(!user) { return done(null, false, req.flash('loginMessage', 'No user found')); }
+        if(!user.validPassword(password)) { return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); }
+
+        return done(null, user);
+      });
+    }
+));
+
+};
---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const bcrypt   = require('bcrypt-nodejs');
+
+const userSchema = mongoose.Schema({
+  email: String,
+  password: String
+});
+
+// generating a hash
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema);
